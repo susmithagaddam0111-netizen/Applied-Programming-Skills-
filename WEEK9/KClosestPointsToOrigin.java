e9b2c9782c128ab6931ac915c29ddabd382faa69
susmithagaddam0111-netizen <susmitha.gaddam0111@gmail.com>
2026-04-21 12:17:42 +0530

    Create KClosestPointsToOrigin.java

---
+++ WEEK9/KClosestPointsToOrigin.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+
+        // Max Heap based on distance
+        PriorityQueue<int[]> pq = new PriorityQueue<>(
+            (a, b) -> (b[0]*b[0] + b[1]*b[1]) - (a[0]*a[0] + a[1]*a[1])
+        );
+
+        for (int[] p : points) {
+            pq.offer(p);
+
+            if (pq.size() > k) {
+                pq.poll(); // remove farthest
+            }
+        }
+
+        int[][] result = new int[k][2];
+
+        for (int i = 0; i < k; i++) {
+            result[i] = pq.poll();
+        }
+
+        return result;
+    }
+}
