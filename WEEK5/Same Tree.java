0b03a1183a83a47c30fe49df59b0ea43d7f20d47
susmithagaddam0111-netizen <susmitha.gaddam0111@gmail.com>
2026-04-21 11:26:59 +0530

    Implement isSameTree method to compare two trees

---
+++ WEEK5/Same Tree.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+
+        // Case 1: both nodes are null
+        if (p == null && q == null) {
+            return true;
+        }
+
+        // Case 2: one is null OR values are different
+        if (p == null || q == null || p.val != q.val) {
+            return false;
+        }
+
+        // Case 3: check left and right subtrees
+        return isSameTree(p.left, q.left) &&
+               isSameTree(p.right, q.right);
+    }
+}
